9c48ca230f175cfc63f17680b3f5abcb17f23492
AustinJohnAaro <austin1472a@gmail.com>
2021-07-25 21:39:54 -0700

    dothesewindowsopen

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -11,6 +11,33 @@ import {
   TOGGLE_CART
 } from '../utils/actions';
 
+test('UPDATE_CATEGORIES', () => {
+  let newState = reducer(initialState, {
+    type: UPDATE_CATEGORIES,
+    categories: [{}, {}]
+  });
+
+  expect(newState.categories.length).toBe(2);
+  expect(initialState.categories.length).toBe(1);
+}); 
+
+
+// import our actions
+import {
+  UPDATE_PRODUCTS,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY
+} from '../utils/actions';
+
+// create a sample of what our global state will look like
+const initialState = {
+  products: [],
+  categories: [{ name: 'Food' }],
+  currentCategory: '1',
+}; 
+
+
+
 const initialState = {
   products: [],
   cart: [
@@ -123,6 +150,18 @@ test('CLEAR_CART', () => {
   expect(newState.cart.length).toBe(0);
   expect(initialState.cart.length).toBe(2);
 });
+
+test('UPDATE_PRODUCTS', () => {
+  let newState = reducer(initialState, {
+    type: UPDATE_PRODUCTS,
+    products: [{}, {}]
+  });
+
+  expect(newState.products.length).toBe(2);
+  expect(initialState.products.length).toBe(0);
+}); 
+
+
 
 test('TOGGLE_CART', () => {
   let newState = reducer(initialState, {
